77014ab77a2aa6f49274354377e50adc69f99026
shiva06031998 <80663993+shiva06031998@users.noreply.github.com>
2021-10-29 00:23:26 +0530

    Add files via upload

---
+++ ex1.rb
@@ -0,0 +1,14 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "This is Shiva kumar"
+name="Shiva Kumar"
+# which denote comment lines
+a=1
+puts "The number #{a}"
+# Line number 14 which hold variable #{name}
+puts "Hi Hello #{name}"
